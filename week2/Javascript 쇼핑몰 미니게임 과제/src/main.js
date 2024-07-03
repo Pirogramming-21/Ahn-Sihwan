eacff6e7b04886c980c2fc92458dbc879a9a5d1f
shwn <zxz8673@gmail.com>
2024-07-04 02:27:08 +0900

    handling events

--- week2/Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ week2/Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,14 +5,14 @@ function loadItems() {
     .then(json => json.items);
 }
 
-// Update the list wiht the given items
+// 리스트 업데이트
 function displayItems(items) {
     const container = document.querySelector('.items');
     const html = items.map(item => createHTMLString(item)).join('');
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
-// Create HTML list item from the given data item
+// HTML 만들기
 function createHTMLString(item) {
     return `
     <li class="item">
@@ -22,10 +22,32 @@ function createHTMLString(item) {
     `;
 }
 
+// 버튼 클릭 시 이벤트
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key ==null || value == null) {
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+// 이벤트 실행 - 로고, 버튼
+function setEventListners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', () => onButtonClick(event, items));
+}
+
 /// main
 loadItems()
  .then(items => {
     displayItems(items);
-        // setEventListners(items)
+    setEventListners(items);
     })
  .catch(console.log)
