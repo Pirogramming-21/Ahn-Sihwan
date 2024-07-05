26e605c36a46426a7e5ff28176410f05777ec00b
shwn <zxz8673@gmail.com>
2024-07-06 04:12:41 +0900

    4단계

--- week2/python_problem/python_problem_1.py
+++ week2/python_problem/python_problem_1.py
@@ -2,10 +2,13 @@ num = 0
 
 while True:
     try:
-        call_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if call_count in [1, 2, 3]:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        if num in [1, 2, 3]:
             break
         else:
             print("1, 2, 3 중 하나를 입력하세요")
     except ValueError:
         print("정수를 입력하세요")
+
+for i in range(1, num + 1):
+    print(f"playerA : {i}")
