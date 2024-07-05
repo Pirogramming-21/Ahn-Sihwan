9bfc1b7f63dc12282cb857fe6d3d2b580651c114
shwn <zxz8673@gmail.com>
2024-07-06 04:35:05 +0900

    6단계

--- week2/python_problem/python_problem_1.py
+++ week2/python_problem/python_problem_1.py
@@ -1,29 +1,27 @@
 num = 0
+player = 'playerA'
 
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if num in [1, 2, 3]:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
+while num < 31:
+    print(player)
+    while True:
+        try:
+            call_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if call_count in [1, 2, 3]:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
 
-for i in range(1, num + 1):
-    print(f"playerA : {i}")
+    for i in range(num + 1, num + call_count + 1):
+        print(f"{player} : {i}")
 
+    num += call_count
 
-while True:
-    try:
-        call_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if call_count in [1, 2, 3]:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
-
-num += call_count
-for i in range(1, call_count + 1):
-    print(f"playerB : {num - call_count + i}")
+    if num >= 31:
+        break
+    
+    if player == 'playerA':
+        player = 'playerB'
+    else:
+        player = 'playerA'
