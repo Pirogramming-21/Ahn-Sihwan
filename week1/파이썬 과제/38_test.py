0cb129dcd47530e1caadcf5d519c6df2da066fda
shwn <zxz8673@gmail.com>
2024-07-06 00:28:59 +0900

    38-7

---
+++ week1/파이썬 과제/38_test.py
@@ -0,0 +1,23 @@
+# try:
+#     file = open('maria.txt', 'r')
+# except FileNotFoundError:  
+#     print('파일이 없습니다.')
+# else:     
+#     s = file.read()
+#     file.close()
+
+class NotPalindromeError(Exception):
+    def __init__(self):
+        super().__init__('회문이 아닙니다.')
+
+def palindrome(word):
+    if(word == word[::-1]):
+        print(word)
+    else:
+        raise NotPalindromeError
+
+try:
+    word = input()
+    palindrome(word)
+except NotPalindromeError as e:
+    print(e)
