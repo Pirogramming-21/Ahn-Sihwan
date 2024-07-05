02a1032e8e86b50cd022eee9c13373f2cdb27e6d
shwn <zxz8673@gmail.com>
2024-07-06 04:17:17 +0900

    5단계

--- week2/python_problem/python_problem_1.py
+++ week2/python_problem/python_problem_1.py
@@ -12,3 +12,18 @@ while True:
 
 for i in range(1, num + 1):
     print(f"playerA : {i}")
+
+
+while True:
+    try:
+        call_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        if call_count in [1, 2, 3]:
+            break
+        else:
+            print("1, 2, 3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")
+
+num += call_count
+for i in range(1, call_count + 1):
+    print(f"playerB : {num - call_count + i}")
