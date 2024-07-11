dcc9cb7c1a8309dcf157f1ce433e0e890abaf413
shwn <zxz8673@gmail.com>
2024-07-11 17:03:13 +0900

    [ADD] 모델 등록

--- week3/myMovieReviews/movielog/models.py
+++ week3/myMovieReviews/movielog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    director = models.CharField(max_length=100)
+    main_actor = models.CharField(max_length=100)
+    running_time = models.IntegerField()
+    review_content = models.TextField()
