9964852bfc29cc0898b5415a3e08a397baf2bb46
shwn <zxz8673@gmail.com>
2024-07-17 00:49:15 +0900

    [add] devtools

--- week4/SWIDEA_SITE/apps/devtools/urls.py
+++ week4/SWIDEA_SITE/apps/devtools/urls.py
@@ -4,5 +4,9 @@ from .views import *
 app_name = 'devtools'
 
 urlpatterns = [
-#   path('', main, name='main'),
+    path('', main, name='main'),
+    path('register/', register, name='register'),
+    path('<int:pk>/', detail, name='detail'),
+    path('<int:pk>/delete/', delete, name='delete'),
+    path('<int:pk>/update/', update, name='update'),
 ]
--- week4/SWIDEA_SITE/apps/devtools/views.py
+++ week4/SWIDEA_SITE/apps/devtools/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Devtool
+from .forms import DevtoolForm
 
-# Create your views here.
+def main(req):
+    devtools = Devtool.objects.all()
+    ctx = {'devtools':devtools}
+    return render(req, 'devtools/list.html', ctx)
+
+def register(req):
+    if req.method == 'GET':
+        form = DevtoolForm()
+        ctx = {'form': form}
+        return render(req, 'devtools/register.html', ctx)
+    form = DevtoolForm(req.POST, req.FILES)
+    if form.is_valid():
+        form.save()
+    return redirect('devtools:main')
+
+def detail(req,pk):
+    devtool = Devtool.objects.get(id=pk)
+    ctx = {'devtool': devtool}
+    return render(req, 'devtools/detail.html', ctx)
+
+def delete(req,pk):
+    if req.method == 'POST':
+        Devtool.objects.get(id=pk).delete()
+    return redirect('devtools:main')
+
+def update(req, pk):
+    devtool = Devtool.objects.get(id=pk)
+    if req.method == 'GET':
+        form = DevtoolForm(instance=devtool)
+        ctx = {'form': form, 'pk': pk}
+        return render(req, 'devtools/update.html', ctx)
+    form = DevtoolForm(req.POST, req.FILES, instance=devtool)
+    if form.is_valid():
+        form.save()
+    return redirect('devtools:main')
