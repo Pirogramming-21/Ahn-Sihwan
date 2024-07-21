96d6da95fdfe521fb154ef6a8ecf428c0d0e856e
shwn <zxz8673@gmail.com>
2024-07-21 23:11:15 +0900

    [add] 자잘한 기능, 디자인 수정

--- week4/piro21_Ajax_Pirostagram/apps/pirostagram/urls.py
+++ week4/piro21_Ajax_Pirostagram/apps/pirostagram/urls.py
@@ -5,6 +5,7 @@ app_name = 'pirostagram'  # URL 네임스페이스 추가
 
 urlpatterns = [
     path('', posts, name='posts'),
+    path('new_post/', new_post, name='new_post'),
     path('like/', like_post, name='like_post'),
     path('comment/', leave_comment, name='leave_comment'),
     path('delete_comment/', delete_comment, name='delete_comment'),
--- week4/piro21_Ajax_Pirostagram/apps/pirostagram/views.py
+++ week4/piro21_Ajax_Pirostagram/apps/pirostagram/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import *
 import json
 
 # Create your views here.
@@ -11,6 +13,36 @@ def posts(request):
     'posts': posts
   }
   return render(request, 'pirostagram/list.html', ctx)
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        image_form = PostImageForm(request.POST, request.FILES)
+
+        if form.is_valid() and image_form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+            images = image_form.cleaned_data.get('images')
+            if images:
+                for img in images:
+                    Image.objects.create(post=post, image=img)
+            
+            messages.success(request, 'Post created successfully!')
+            return redirect('pirostagram:posts')
+        else:
+            messages.error(request, 'There was an error with your submission. Please check the form.')
+    else:
+        form = PostForm()
+        image_form = PostImageForm()
+    
+    ctx = {
+        'form': form,
+        'image_form': image_form
+    }
+    return render(request, 'pirostagram/new_post.html', ctx)
 
 @login_required
 def like_post(request):
